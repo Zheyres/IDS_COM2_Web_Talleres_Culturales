479eac6a9aa922f9ad52c5644009d30f051c513d
Gonzalo Hitta <dezolator94@gmail.com>
2026-04-17 15:10:26 -0300

    fixed form

--- scripts/register.js
+++ scripts/register.js
@@ -1,4 +1,59 @@
-document.getElementById("btnRegistrar").addEventListener("click", () => {
-  window.location.href = "../Pages/Register.html";
-});
+document.addEventListener("DOMContentLoaded", () => {
 
+  //  BOTÓN REGISTRAR 
+  const btnRegistrar = document.getElementById("btnRegistrar");
+  if (btnRegistrar) {
+    btnRegistrar.addEventListener("click", () => {
+      window.location.href = "../Pages/register.html";
+    });
+  }
+
+  //  DIRECCIÓN
+  const checkbox = document.getElementById("mismaDireccion");
+  const direccionInput = document.getElementById("direccion");
+
+  if (checkbox && direccionInput) {
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        direccionInput.disabled = true;
+        direccionInput.removeAttribute("required");
+        direccionInput.value = "Centro cultural";
+      } else {
+        direccionInput.disabled = false;
+        direccionInput.setAttribute("required", true);
+      }
+    });
+  }
+
+  //  IMAGEN
+  const inputImagen = document.getElementById("imagenInput");
+  const preview = document.getElementById("previewImagen");
+  const nombre = document.getElementById("nombreArchivo");
+  const btnImagen = document.getElementById("btnImagen");
+
+  if (btnImagen && inputImagen) {
+    btnImagen.addEventListener("click", () => {
+      inputImagen.click();
+    });
+  }
+
+  if (inputImagen && preview && nombre) {
+    inputImagen.addEventListener("change", () => {
+      const file = inputImagen.files[0];
+
+      if (file) {
+        nombre.textContent = file.name;
+
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+          preview.src = e.target.result;
+          preview.style.display = "block";
+        };
+
+        reader.readAsDataURL(file);
+      }
+    });
+  }
+
+});
